416c1fc0bb4c5b838b7b94434899a5b5ffc99ac0
fcogomezcal <fcogomezcal@gmail.com>
2026-06-30 17:18:08 -0500

    Modificación con el slider y visualización corregido

--- controller/pokemon.php
+++ controller/pokemon.php
@@ -9,17 +9,22 @@
 
     switch($_GET["op"]){
         case "listar":
-            $datos=$pokemon->get_pokemon(@$_POST["tipo"], @$_POST["nombre"]); //en get_pokemon debemos pasarle el valor de tipo
+            $datos=$pokemon->get_pokemon(@$_POST["tipo"], @$_POST["nombre"]);
             foreach($datos as $row){
                 ?>
-                    <div class="col-sm-4 col-lg-3 col-md-3">
-                        <div class="pokemon text-center">
-                            <img src="<?php echo $row["imagengif"] ?>" alt="" class="img-responsive" height="50" width="50">
-                            <p><?php echo $row["number"] ?></p>
-                            <h4><?php echo $row["nombre"] ?></h4>
-                            <h5><?php echo $row["tipo"] ?></h5>
-                            <p>Ataque: <?php echo $row["attack"] ?></p>
-                            <p>Defensa: <?php echo $row["defense"] ?></p>
+                    <div class="col-sm-4 col-md-3 col-lg-3 mb-3">
+                        <div class="pokemon text-center p-2 border rounded shadow-sm h-100 text-break overflow-hidden" style="background-color: #fff;">
+                            
+                            <img src="<?php echo $row["imagengif"] ?>" alt="gif" class="img-fluid mt-2" style="max-height: 60px; width: auto;">
+                            
+                            <p class="text-muted small mt-2 mb-1">#<?php echo $row["number"] ?></p>
+                            
+                            <h4 class="text-wrap" style="font-size: 1.1rem; font-weight: bold;"><?php echo $row["nombre"] ?></h4>
+                            
+                            <span class="badge bg-secondary mb-2"><?php echo $row["tipo"] ?></span>
+                            
+                            <p class="mb-0" style="font-size: 0.9rem;">Ataque: <?php echo $row["attack"] ?></p>
+                            <p class="mb-0" style="font-size: 0.9rem;">Defensa: <?php echo $row["defense"] ?></p>
                         </div>
                     </div>
                 <?php
@@ -31,16 +36,17 @@
             $datos=$pokemon->get_tipo();
             foreach($datos as $row){
                 ?>
-                    <div class="list-group-item checkbox">
-                        <label> <input type="checkbox" class="filtrodetalle" name="tipos" value="<?php echo $row["tipo"] ?>"> <?php echo $row["tipo"] ?> </label>
-                    </div>
+                    <label class="list-group-item d-flex gap-2">
+                        <input type="checkbox" class="filtrodetalle form-check-input flex-shrink-0" name="tipos" value="<?php echo $row["tipo"] ?>"> 
+                        <span>
+                            <?php echo $row["tipo"] ?>
+                        </span>
+                    </label>
                 <?php
                 
             }
             break;
 
     }
-
-
 
 ?>
--- view/ListPokemon/index.php
+++ view/ListPokemon/index.php
@@ -3,7 +3,9 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-slider@10.6.2/dist/css/bootstrap-slider.min.css">
+    
     <link rel="stylesheet" href="../../assets/style.css">
     <title>AnderCode</title>
 </head>
@@ -23,9 +25,17 @@
                 </div>
                 <br>
                 <div class="list-group">
-                    <h4>Tipos</h4>
+                    <h4>Tipo</h4>
                     <div id="listtipo">
                         
+                    </div>
+                </div>
+                <br>
+                <div class="list-group">
+                    <h4>Ataque</h4>
+                    <div class="list-group-item">
+                        <input id="txtataque" type="text" data-slider-min="0" data-slider-max="155" data-slider-step="1" data-slider-value="155" value="155"/>
+                        <div id="sliderValue"></div>
                     </div>
                 </div>
             </div>
@@ -38,8 +48,10 @@
     </div>
 </body>
 
-<script src="https://code.jquery.com/jquery-4.0.0.min.js" integrity="sha256-OaVG6prZf4v69dPg6PhVattBXkcOWQB62pdZ3ORyrao=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-slider@10.6.2/dist/bootstrap-slider.min.js"></script>
+
 <script src="listpokemon.js"></script>
 
 
